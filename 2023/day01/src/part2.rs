d07249715f9585b68f72354cead66437a67aaba6
Zan Baldwin <hello@zanbaldwin.com>
2023-12-02 13:04:29 +0100

    Complete Day01 Part 2

--- 2023/day01/src/part2.rs
+++ 2023/day01/src/part2.rs
@@ -1,10 +1,27 @@
 use crate::aoc_error::AocError;
+use std::iter;
 
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    Err(AocError::IoError(::std::io::Error::new(
-        ::std::io::ErrorKind::Other, 
-        "Not yet implemented.",
-    )))
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let total: u32 = input.lines().map(|line: &str| -> u32 {
+        let mut index = 0;
+        let words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        let line_iter = iter::from_fn(move || {
+            let check_line = &line[index..];
+            let mut result: Option<char> = None;
+            'found: for (i, word) in words.iter().enumerate() {
+                if check_line.starts_with(word) {
+                    result = Some((i + 1).to_string().chars().nth(0).unwrap());
+                    break 'found;
+                }
+            }
+            index += 1;
+            result.or_else(|| check_line.chars().next())
+        });
+    
+        let nums: Vec<u32> = line_iter.filter_map(|c| c.to_digit(10)).collect();
+        (nums.first().unwrap() * 10) + nums.last().unwrap()
+    }).sum();
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
