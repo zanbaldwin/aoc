477f65393c05d91991b1377425cf217af4837e26
Zan Baldwin <hello@zanbaldwin.com>
2023-12-02 13:30:24 +0100

    Remove load_file Dependency
    Use the include_str!() macro from the standard library instead.

--- 2023/day02/src/bin/d2p1.rs
+++ 2023/day02/src/bin/d2p1.rs
@@ -1,12 +1,11 @@
 use day02::part1::process;
 use miette::Context;
-use load_file::load_str;
 
 #[tracing::instrument]
 fn main() -> miette::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let file = load_str!("../../input.txt");
+    let file = include_str!("../../input.txt");
     let result = process(file).context("Process Part 1");
     println!("Day 02; Part 1: The result is {}.", result?);
     Ok(())
--- 2023/day02/src/bin/d2p2.rs
+++ 2023/day02/src/bin/d2p2.rs
@@ -1,12 +1,11 @@
 use day02::part2::process;
 use miette::Context;
-use load_file::load_str;
 
 #[tracing::instrument]
 fn main() -> miette::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let file = load_str!("../../input.txt");
+    let file = include_str!("../../input.txt");
     let result = process(file).context("Process Part 2");
     println!("Day 02; Part 2: The result is {}.", result?);
     Ok(())
