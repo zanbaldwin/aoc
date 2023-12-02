10a7cbfb62b0bcfd56aeebb8761e418c7f4165c8
Zan Baldwin <hello@zanbaldwin.com>
2023-12-02 13:02:43 +0100

    Add Tests for Day01 Part 2

--- 2023/day01/src/part2.rs
+++ 2023/day01/src/part2.rs
@@ -10,4 +10,16 @@ pub fn process(_input: &str) -> miette::Result<String, AocError> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part2() {
+        let input: &'static str = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
+        assert_eq!("281", process(input).unwrap());
+    }
+
+    #[test]
+    fn test_word_order() {
+        let line = "xtwone3four";
+        assert_eq!("24", process(line).unwrap());
+    }
 }
