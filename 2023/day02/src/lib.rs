731561e91061681e55d8281d2243db2821cdc321
Zan Baldwin <hello@zanbaldwin.com>
2023-12-02 15:08:45 +0100

    Complete Day02 Part 1
    Also run Rust Formatter.

--- 2023/day02/src/lib.rs
+++ 2023/day02/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod aoc_error;
+pub(crate) mod models;
+pub(crate) mod parser;
 pub mod part1;
 pub mod part2;
-pub mod aoc_error;
--- 2023/day02/src/part2.rs
+++ 2023/day02/src/part2.rs
@@ -2,7 +2,7 @@ use crate::aoc_error::AocError;
 
 pub fn process(_input: &str) -> miette::Result<String, AocError> {
     Err(AocError::IoError(::std::io::Error::new(
-        ::std::io::ErrorKind::Other, 
+        ::std::io::ErrorKind::Other,
         "Not yet implemented.",
     )))
 }
