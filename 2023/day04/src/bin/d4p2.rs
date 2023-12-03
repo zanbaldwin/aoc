cefb5ba283a564cc634ac39fdea9566a22150ee9
Zan Baldwin <hello@zanbaldwin.com>
2023-12-03 15:40:35 +0100

    Create Day04 Ready for Tomorrow

---
+++ 2023/day04/src/bin/d4p2.rs
@@ -0,0 +1,12 @@
+use day04::part2::process;
+use miette::Context;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input.txt");
+    let result = process(file).context("Process Part 2");
+    println!("Day 03; Part 2: The result is {}.", result?);
+    Ok(())
+}
