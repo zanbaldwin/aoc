8d75d004fa1f235b96406da8cea9a37b7937a874
Zan Baldwin <hello@zanbaldwin.com>
2023-12-04 15:26:37 +0100

    Day04: Write Input Parser

--- 2023/day04/src/lib.rs
+++ 2023/day04/src/lib.rs
@@ -2,9 +2,30 @@ use aoc_error::AocError;
 use std::io::{Error, ErrorKind};
 
 pub mod aoc_error;
+mod parser;
 pub mod part1;
 pub mod part2;
 
 fn invalid_input_error(message: &str) -> AocError {
     AocError::IoError(Error::new(ErrorKind::InvalidInput, message))
+}
+
+#[derive(Debug)]
+pub(crate) struct Scratchcard {
+    id: u32,
+    winning_numbers: Vec<u32>,
+    playing_numbers: Vec<u32>,
+}
+impl Scratchcard {
+    pub fn new(id: u32, winning_numbers: Vec<u32>, playing_numbers: Vec<u32>) -> Self {
+        Self {
+            id,
+            winning_numbers,
+            playing_numbers,
+        }
+    }
+
+    pub fn id(&self) -> u32 {
+        self.id
+    }
 }
---
+++ 2023/day04/src/parser.rs
@@ -0,0 +1,63 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, line_ending, space0, space1},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{separated_pair, tuple},
+    IResult,
+};
+
+use crate::{aoc_error::AocError, invalid_input_error, Scratchcard};
+
+fn parse_list_of_numbers(input: &str) -> IResult<&str, Vec<u32>> {
+    map(separated_list1(space1, digit1), |numbers| {
+        numbers
+            .iter()
+            // We already know that the &str only contain digits because of the
+            // parser before this. Unwrap.
+            .map(|number: &&str| number.parse().unwrap())
+            .collect()
+    })(input)
+}
+
+fn parse_scratchcard(input: &str) -> IResult<&str, Scratchcard> {
+    map(
+        tuple((
+            tag("Card"),
+            space1,
+            digit1,
+            tag(":"),
+            space1,
+            separated_pair(
+                parse_list_of_numbers,
+                tuple((space0, tag("|"), space0)),
+                parse_list_of_numbers,
+            ),
+        )),
+        |(_, _, card_number, _, _, (winning_numbers, playing_numbers))| {
+            Scratchcard::new(
+                // Already know that card_numbers only contain digits because of
+                // previous parser. Just unwrap.
+                card_number.parse().unwrap(),
+                winning_numbers,
+                playing_numbers,
+            )
+        },
+    )(input)
+}
+
+/// Parse Input from AoC2023 Day 4 into Data Structure
+pub(crate) fn parse(input: &str) -> miette::Result<Vec<Scratchcard>, AocError> {
+    match separated_list1(line_ending, parse_scratchcard)(input.trim()) {
+        Ok((remaining_input, scratchcards)) => {
+            if !remaining_input.trim().is_empty() {
+                Err(invalid_input_error(
+                    "Additional unparsed data at the end of input: {remaining_input}",
+                ))
+            } else {
+                Ok(scratchcards)
+            }
+        }
+        Err(_) => Err(invalid_input_error("Input could not be correctly parsed.")),
+    }
+}
