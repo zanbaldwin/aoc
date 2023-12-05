74b42cacc3181e1ce1ff5c030242eba50e8ef606
Zan Baldwin <hello@zanbaldwin.com>
2023-12-05 13:06:03 +0100

    Update Day Template: Move Reusable Pieces to Common Dependency

--- 2023/day-template/src/lib.rs
+++ 2023/day-template/src/lib.rs
@@ -1,10 +1,4 @@
-use aoc_error::AocError;
-use std::io::{Error, ErrorKind};
+use common::AocError;
 
-pub mod aoc_error;
 pub mod part1;
 pub mod part2;
-
-fn invalid_input_error(message: &str) -> AocError {
-    AocError::IoError(Error::new(ErrorKind::InvalidInput, message))
-}
