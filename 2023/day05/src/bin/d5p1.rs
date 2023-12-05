b8dee4041d3dfa8373d836ef30ed883049f27d02
Zan Baldwin <hello@zanbaldwin.com>
2023-12-05 14:06:33 +0100

    Setup Day05

---
+++ 2023/day05/src/bin/d5p1.rs
@@ -0,0 +1,12 @@
+use day05::part1::process;
+use miette::Context;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input.txt");
+    let result = process(file).context("Process Part 1");
+    println!("Day 05; Part 1: The result is {}.", result?);
+    Ok(())
+}
---
+++ 2023/day05/src/part1.rs
@@ -0,0 +1,48 @@
+use common::AocError;
+
+pub fn process(_input: &str) -> miette::Result<String, AocError> {
+    Err(AocError::from_str("Not yet implemented."))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+        assert_eq!("35", process(input).unwrap());
+    }
+}
