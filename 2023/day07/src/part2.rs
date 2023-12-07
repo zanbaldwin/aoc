e75c514fd3bb2c8a271306df6d3f38df054615cd
Zan Baldwin <hello@zanbaldwin.com>
2023-12-07 17:37:00 +0100

    Add Unit Tests for Day07 Part 2

--- 2023/day07/src/part2.rs
+++ 2023/day07/src/part2.rs
@@ -7,4 +7,40 @@ pub fn process(_input: &str) -> miette::Result<String, AocError> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const DAY07_TEST_INPUT: &str = "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483";
+
+    #[test]
+    fn test_part2() {
+        assert_eq!("5905", process(DAY07_TEST_INPUT).unwrap());
+    }
+
+    #[test]
+    fn test_part2_reddit() {
+        assert_eq!(
+            "6839",
+            process(
+                "2345A 1
+Q2KJJ 13
+Q2Q2Q 19
+T3T3J 17
+T3Q33 11
+2345J 3
+J345A 2
+32T3K 5
+T55J5 29
+KK677 7
+KTJJT 34
+QQQJA 31
+JJJJJ 37
+JAAAA 43
+AAAAJ 59
+AAAAA 61
+2AAAA 23
+2JJJJ 53
+JJJJ2 41"
+            )
+            .unwrap()
+        );
+    }
 }
