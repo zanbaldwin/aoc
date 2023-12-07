192303c8449bbeac80af49381f2c29040ccee40b
Zan Baldwin <hello@zanbaldwin.com>
2023-12-07 19:48:12 +0100

    Feeble Attempt at Part 2
    Pretty sure I took the wrong approach right from the start and need to think of a completely different approach.

--- 2023/day07/src/bin/d7p1.rs
+++ 2023/day07/src/bin/d7p1.rs
@@ -1,12 +1,10 @@
-use day07::part1::process;
-use miette::Context;
+use day07::{part1::process, Error};
 
 #[tracing::instrument]
-fn main() -> miette::Result<()> {
+fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
-
     let file = include_str!("../../input.txt");
-    let result = process(file).context("Process Part 1");
-    println!("Day 07; Part 1: The result is {}.", result?);
+    let result = process(file)?;
+    println!("Day 07; Part 1: The result is {result}.");
     Ok(())
 }
--- 2023/day07/src/lib.rs
+++ 2023/day07/src/lib.rs
@@ -2,14 +2,22 @@ mod display;
 pub mod part1;
 pub mod part2;
 
-mod models {
-    use std::{cmp::Ordering, collections::HashMap};
-
-    #[derive(Debug)]
-    pub(crate) enum Error {
-        InvalidCardCharacter(char),
-        WrongNumberOfCards(usize),
+#[derive(Debug)]
+pub enum Error {
+    NotYetImplemented,
+    Other(String),
+    InvalidCardCharacter(char),
+    WrongNumberOfCards(usize),
+}
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Self::Other(value.to_string())
     }
+}
+
+mod models {
+    use super::Error;
+    use std::{cmp::Ordering, collections::HashMap};
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
     pub(crate) enum Card {
@@ -49,7 +57,83 @@ mod models {
         }
     }
 
-    #[derive(Debug, PartialEq, Eq, PartialOrd)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub(crate) enum JokeredCard {
+        Jack,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Queen,
+        King,
+        Ace,
+    }
+    impl TryFrom<char> for JokeredCard {
+        type Error = Error;
+        fn try_from(value: char) -> Result<Self, Self::Error> {
+            Ok(match value {
+                '2' => Self::Two,
+                '3' => Self::Three,
+                '4' => Self::Four,
+                '5' => Self::Five,
+                '6' => Self::Six,
+                '7' => Self::Seven,
+                '8' => Self::Eight,
+                '9' => Self::Nine,
+                'T' => Self::Ten,
+                'J' => Self::Jack,
+                'Q' => Self::Queen,
+                'K' => Self::King,
+                'A' => Self::Ace,
+                c => return Err(Error::InvalidCardCharacter(c)),
+            })
+        }
+    }
+    impl From<Card> for JokeredCard {
+        fn from(card: Card) -> Self {
+            match card {
+                Card::Two => Self::Two,
+                Card::Three => Self::Three,
+                Card::Four => Self::Four,
+                Card::Five => Self::Five,
+                Card::Six => Self::Six,
+                Card::Seven => Self::Seven,
+                Card::Eight => Self::Eight,
+                Card::Nine => Self::Nine,
+                Card::Ten => Self::Ten,
+                Card::Jack => Self::Jack,
+                Card::Queen => Self::Queen,
+                Card::King => Self::King,
+                Card::Ace => Self::Ace,
+            }
+        }
+    }
+    impl From<JokeredCard> for Card {
+        fn from(card: JokeredCard) -> Self {
+            match card {
+                JokeredCard::Two => Self::Two,
+                JokeredCard::Three => Self::Three,
+                JokeredCard::Four => Self::Four,
+                JokeredCard::Five => Self::Five,
+                JokeredCard::Six => Self::Six,
+                JokeredCard::Seven => Self::Seven,
+                JokeredCard::Eight => Self::Eight,
+                JokeredCard::Nine => Self::Nine,
+                JokeredCard::Ten => Self::Ten,
+                JokeredCard::Jack => Self::Jack,
+                JokeredCard::Queen => Self::Queen,
+                JokeredCard::King => Self::King,
+                JokeredCard::Ace => Self::Ace,
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq, Eq)]
     pub(crate) enum HandType {
         HighCard(Card),
         OnePair(Card),
@@ -60,7 +144,7 @@ mod models {
         FiveOfAKind(Card),
     }
     impl HandType {
-        fn order(&self) -> u8 {
+        fn order(&self) -> usize {
             match self {
                 Self::HighCard(_) => 1,
                 Self::OnePair(_) => 2,
@@ -81,24 +165,19 @@ mod models {
             self.order().cmp(&other.order())
         }
     }
+    impl PartialOrd for HandType {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
     impl TryFrom<Vec<Card>> for HandType {
         type Error = Error;
-        fn try_from(cards: Vec<Card>) -> Result<Self, Error> {
-            if cards.len() != 5 {
-                return Err(Error::WrongNumberOfCards(cards.len()));
-            }
-
-            let mut map: HashMap<Card, u8> = HashMap::new();
+        fn try_from(cards: Vec<Card>) -> Result<Self, Self::Error> {
+            let mut map: HashMap<Card, usize> = HashMap::new();
             for card in cards {
                 *map.entry(card).or_insert(0) += 1;
             }
 
-            map.try_into()
-        }
-    }
-    impl TryFrom<HashMap<Card, u8>> for HandType {
-        type Error = Error;
-        fn try_from(map: HashMap<Card, u8>) -> Result<Self, Self::Error> {
             Ok(match map.len() {
                 1 => {
                     // FiveOfAKind (AAAAA).
@@ -172,58 +251,141 @@ mod models {
             })
         }
     }
+    impl TryFrom<Vec<JokeredCard>> for HandType {
+        type Error = Error;
+        fn try_from(cards: Vec<JokeredCard>) -> Result<Self, Self::Error> {
+            if !cards.contains(&JokeredCard::Jack) {
+                // If the hand doesn't contain any jokers then at least get a
+                // little win wherever possible because part 2 is beginning to
+                // hurt my brain.
+                let cards: Vec<Card> = cards.into_iter().map(|card| card.into()).collect();
+                return HandType::try_from(cards);
+            }
 
-    #[derive(Debug, PartialEq, Eq)]
-    pub(crate) struct Hand {
-        pub(crate) cards: Vec<Card>,
-        pub(crate) hand_type: HandType,
-        pub(crate) bid: u32,
+            todo!()
+        }
     }
-    impl Hand {
-        pub fn new(hand: &str, bid: u32) -> Result<Self, Error> {
+
+    pub(crate) struct ParsedHand {
+        pub(crate) cards: Vec<Card>,
+        pub(crate) bid: usize,
+    }
+    impl ParsedHand {
+        pub fn new(hand: &str, bid: usize) -> Result<Self, Error> {
             let cards: Result<Vec<Card>, Error> = hand.chars().map(|c| c.try_into()).collect();
             let cards = cards?;
-            let hand_type: HandType = cards.clone().try_into()?;
-            Ok(Self {
-                cards,
-                hand_type,
-                bid,
-            })
-        }
-
-        pub fn score(&self, rank: usize) -> u64 {
-            (self.bid as u64) * (rank as u64)
+            if cards.len() != 5 {
+                return Err(Error::WrongNumberOfCards(cards.len()));
+            }
+            Ok(Self { cards, bid })
         }
     }
-    impl Ord for Hand {
+
+    pub(crate) trait Hand: PartialEq + Eq + PartialOrd + Ord + TryFrom<ParsedHand> {
+        fn score(&self, rank: usize) -> usize;
+    }
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub(crate) struct HandWithoutJokers {
+        pub(crate) cards: Vec<Card>,
+        pub(crate) hand_type: HandType,
+        pub(crate) bid: usize,
+    }
+    impl TryFrom<ParsedHand> for HandWithoutJokers {
+        type Error = Error;
+        fn try_from(hand: ParsedHand) -> Result<Self, Self::Error> {
+            let hand_type: HandType = hand.cards.clone().try_into()?;
+            Ok(HandWithoutJokers {
+                cards: hand.cards,
+                hand_type,
+                bid: hand.bid,
+            })
+        }
+    }
+    impl Hand for HandWithoutJokers {
+        fn score(&self, rank: usize) -> usize {
+            self.bid * rank
+        }
+    }
+    // For some reason, the derive macro versions of PartialOrd cause the
+    // ordering to fail. Will look into that another time, I guess.
+    impl PartialOrd for HandWithoutJokers {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+    impl Ord for HandWithoutJokers {
         fn cmp(&self, other: &Self) -> Ordering {
             let hand_order = self.hand_type.cmp(&other.hand_type);
             if hand_order != Ordering::Equal {
                 return hand_order;
             }
-
             for (own_card, other_card) in self.cards.iter().zip(&other.cards) {
                 let card_order = own_card.cmp(other_card);
                 if card_order != Ordering::Equal {
                     return card_order;
                 }
             }
-
             Ordering::Equal
+        }
+    }
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub(crate) struct HandWithJokers {
+        pub(crate) cards: Vec<Card>,
+        pub(crate) hand_type: HandType,
+        pub(crate) bid: usize,
+    }
+    impl TryFrom<ParsedHand> for HandWithJokers {
+        type Error = Error;
+        fn try_from(hand: ParsedHand) -> Result<Self, Self::Error> {
+            let hand_type: HandType = hand
+                .cards
+                .clone()
+                .into_iter()
+                .map(|card| card.into())
+                .collect::<Vec<JokeredCard>>()
+                .try_into()?;
+            Ok(HandWithJokers {
+                cards: hand.cards,
+                hand_type,
+                bid: hand.bid,
+            })
+        }
+    }
+    impl Hand for HandWithJokers {
+        fn score(&self, rank: usize) -> usize {
+            self.bid * rank
         }
     }
     // For some reason, the derive macro versions of PartialOrd cause the
     // ordering to fail. Will look into that another time, I guess.
-    impl PartialOrd for Hand {
+    impl PartialOrd for HandWithJokers {
         fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
             Some(self.cmp(other))
+        }
+    }
+    impl Ord for HandWithJokers {
+        fn cmp(&self, other: &Self) -> Ordering {
+            let hand_order = self.hand_type.cmp(&other.hand_type);
+            if hand_order != Ordering::Equal {
+                return hand_order;
+            }
+            for (own_card, other_card) in self.cards.iter().zip(&other.cards) {
+                let card_order = own_card.cmp(other_card);
+                if card_order != Ordering::Equal {
+                    return card_order;
+                }
+            }
+            Ordering::Equal
         }
     }
 }
 
 mod parser {
+    use crate::Error;
+
     use super::models::*;
-    use common::AocError;
     use nom::{
         bytes::complete::take,
         character::complete::{digit1, line_ending, space1},
@@ -235,15 +397,15 @@ mod parser {
 
     const VALID_CARD_CHARACTERS: &str = "23456789TJQKA";
 
-    pub(crate) fn parse(input: &str) -> miette::Result<Vec<Hand>, AocError> {
+    pub(crate) fn parse(input: &str) -> miette::Result<Vec<ParsedHand>, Error> {
         common::nom(parse_playlist, input)
     }
 
-    fn parse_playlist(input: &str) -> IResult<&str, Vec<Hand>> {
+    fn parse_playlist(input: &str) -> IResult<&str, Vec<ParsedHand>> {
         separated_list1(line_ending, parse_hand)(input)
     }
 
-    fn parse_hand(input: &str) -> IResult<&str, Hand> {
+    fn parse_hand(input: &str) -> IResult<&str, ParsedHand> {
         map(
             separated_pair(
                 verify(take(5usize), |cards: &str| {
@@ -252,7 +414,7 @@ mod parser {
                 space1,
                 digit1,
             ),
-            |(cards, bid)| Hand::new(cards, bid.parse().unwrap()).unwrap(),
+            |(cards, bid)| ParsedHand::new(cards, bid.parse().unwrap()).unwrap(),
         )(input)
     }
 }
