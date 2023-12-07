e3dbbc745dc95e8b57d0f1952370aff51da06b0f
Zan Baldwin <hello@zanbaldwin.com>
2023-12-07 22:17:30 +0100

    Update Day Template

--- 2023/day-template/src/lib.rs
+++ 2023/day-template/src/lib.rs
@@ -1,2 +1,15 @@
 pub mod part1;
 pub mod part2;
+
+#[derive(Debug)]
+pub enum Error {
+    NotYetImplemented,
+    Other(String),
+    InvalidCardCharacter(char),
+    WrongNumberOfCards(usize),
+}
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Self::Other(value.to_string())
+    }
+}
