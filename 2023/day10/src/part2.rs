f558002d7183acb600bda20282f6caab82a3894b
Zan Baldwin <hello@zanbaldwin.com>
2023-12-10 15:49:28 +0100

    Add Unit Tests for Day10 Part 2

--- 2023/day10/src/part2.rs
+++ 2023/day10/src/part2.rs
@@ -1,10 +1,54 @@
-use crate::error::Error;
+use crate::{error::Error, models::*};
 
-pub fn process(_input: &str) -> Result<String, Error> {
+pub fn process(input: &str) -> Result<String, Error> {
     Err(Error::NotYetImplemented)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part2_map1() {
+        let input: &str = "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+        assert_eq!("4", process(input).unwrap());
+    }
+
+    #[test]
+    fn test_part2_map2() {
+        let input: &str = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+        assert_eq!("8", process(input).unwrap());
+    }
+
+    #[test]
+    fn test_part2_map3() {
+        let input: &str = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
+        assert_eq!("10", process(input).unwrap());
+    }
 }
