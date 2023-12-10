ff6dcef2f08c5fab20268404acacceddeda8f9cc
Zan Baldwin <hello@zanbaldwin.com>
2023-12-10 09:37:33 +0100

    Setup Day10

---
+++ 2023/day10/src/error.rs
@@ -0,0 +1,21 @@
+use std::fmt;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    NotYetImplemented,
+    Other(String),
+    InvalidPipeCharacter(char),
+}
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Self::Other(value.to_string())
+    }
+}
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Other(msg) => write!(f, "Error: {}", msg),
+            _ => write!(f, "{:?}", self),
+        }
+    }
+}
