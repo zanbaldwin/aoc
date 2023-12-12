847685c413ad3603f997dac2b82b0d8a1174ad16
Zan Baldwin <hello@zanbaldwin.com>
2023-12-12 22:49:23 +0100

    Complete Day11 Part 2

--- 2023/day11/src/part1.rs
+++ 2023/day11/src/part1.rs
@@ -2,9 +2,9 @@ use crate::{error::Error, models::Universe};
 
 pub fn process(input: &str) -> Result<String, Error> {
     let universe: Universe = input.try_into()?;
-    let expanded = universe.measure().expand(1);
+    let expanded = universe.measure().expand(2);
     let distances = expanded.distances();
-    let total: usize = distances.into_iter().map(|(_, distance)| distance).sum();
+    let total: usize = distances.into_values().sum();
     Ok(total.to_string())
 }
 
@@ -27,7 +27,4 @@ mod tests {
     fn test_part1() {
         assert_eq!("374", process(GALACTICAL_MAP).unwrap());
     }
-
-    #[test]
-    fn test_expansion() {}
 }
--- 2023/day11/src/part2.rs
+++ 2023/day11/src/part2.rs
@@ -1,10 +1,50 @@
-use crate::error::Error;
+use crate::{error::Error, models::Universe};
 
-pub fn process(_input: &str) -> Result<String, Error> {
-    Err(Error::NotYetImplemented)
+pub fn process(input: &str) -> Result<String, Error> {
+    let universe: Universe = input.try_into()?;
+    let expanded = universe.measure().expand(1_000_000);
+    let distances = expanded.distances();
+    let total: usize = distances.into_values().sum();
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const GALACTICAL_MAP: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+    #[test]
+    fn test_part1_ten() {
+        let universe: Universe = GALACTICAL_MAP.try_into().unwrap();
+        let expanded = universe.measure().expand(10);
+        println!("{expanded}");
+        let total: usize = expanded
+            .distances()
+            .into_iter()
+            .map(|(_, distance)| distance)
+            .sum();
+        assert_eq!(1030, total);
+    }
+
+    #[test]
+    fn test_part1_hundred() {
+        let universe: Universe = GALACTICAL_MAP.try_into().unwrap();
+        let expanded = universe.measure().expand(100);
+        let total: usize = expanded
+            .distances()
+            .into_iter()
+            .map(|(_, distance)| distance)
+            .sum();
+        assert_eq!(8410, total);
+    }
 }
