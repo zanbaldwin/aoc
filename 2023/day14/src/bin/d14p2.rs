93b8b2db61106ff93c85f9d94dcc9963b91d88b1
Zan Baldwin <hello@zanbaldwin.com>
2023-12-14 21:02:04 +0100

    Complete Day14 Part 1

---
+++ 2023/day14/src/bin/d14p2.rs
@@ -0,0 +1,10 @@
+use day14::{error::Error, part2::process};
+
+#[tracing::instrument]
+fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+    let file = include_str!("../../input.txt");
+    let result = process(file)?;
+    println!("Day 14; Part 2: The result is {result}.");
+    Ok(())
+}
