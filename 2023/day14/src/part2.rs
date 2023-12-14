066d0bf57df3bb333d01689841ffbd7993677f79
Zan Baldwin <hello@zanbaldwin.com>
2023-12-14 23:39:27 +0100

    Completed, but Unanswered, Day 14 Part 2

--- 2023/day14/src/part2.rs
+++ 2023/day14/src/part2.rs
@@ -1,19 +1,8 @@
-use crate::{error::Error, models::Platform, Tilt, TiltDirection};
+use crate::{error::Error, models::Platform, Spin};
 
 pub fn process(input: &str) -> Result<String, Error> {
     let mut platform: Platform = input.trim().try_into()?;
-
-    [
-        TiltDirection::North,
-        TiltDirection::West,
-        TiltDirection::South,
-        TiltDirection::East,
-    ]
-    .iter()
-    .cycle()
-    .take(1_000)
-    .for_each(|direction| platform.tilt(direction));
-
+    platform.spin(1_000);
     Ok(platform.total_load().to_string())
 }
 
@@ -35,6 +24,65 @@ O.#..O.#.#
 
     #[test]
     fn test_part2() {
-        assert_eq!("64", process(TEST_INPUT).unwrap());
+        let mut platform: Platform = TEST_INPUT.try_into().unwrap();
+        platform.spin(1_000);
+        assert_eq!("64", platform.total_load().to_string());
+    }
+
+    const ONE_SPIN: &str = "
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....";
+
+    #[test]
+    fn test_one_spin() {
+        let mut platform: Platform = TEST_INPUT.try_into().unwrap();
+        platform.spin(1);
+        assert_eq!(ONE_SPIN.trim(), format!("{platform}").trim());
+    }
+
+    const TWO_SPIN: &str = "
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O";
+
+    #[test]
+    fn test_two_spin() {
+        let mut platform: Platform = TEST_INPUT.try_into().unwrap();
+        platform.spin(2);
+        assert_eq!(TWO_SPIN.trim(), format!("{platform}").trim());
+    }
+
+    const THREE_SPIN: &str = "
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O";
+
+    #[test]
+    fn test_three_spin() {
+        let mut platform: Platform = TEST_INPUT.try_into().unwrap();
+        platform.spin(3);
+        assert_eq!(THREE_SPIN.trim(), format!("{platform}").trim());
     }
 }
