195f249285082a0845e3a8f21d3aaa120b69d286
Zan Baldwin <hello@zanbaldwin.com>
2023-12-15 17:27:29 +0100

    Update Day Template

--- 2023/day-template/src/error.rs
+++ 2023/day-template/src/error.rs
@@ -18,3 +18,24 @@ impl fmt::Display for Error {
         }
     }
 }
+
+// If today's Nom parsers have scenarios where they need to throw custom errors,
+// then implement these on the Error enum:
+//
+// use nom::error::{ErrorKind, FromExternalError, ParseError};
+// impl ParseError<&str> for Error {
+//     fn from_error_kind(_input: &str, kind: ErrorKind) -> Self {
+//         Self::Nom(kind)
+//     }
+//
+//     fn append(_input: &str, _kind: ErrorKind, other: Self) -> Self {
+//         other
+//     }
+// }
+// // Required to implement trait `FromExternalError` to use custom errors with
+// // functions like `nom::combinator::map_res()`.
+// impl<E> FromExternalError<&str, E> for Error {
+//     fn from_external_error(_input: &str, kind: ErrorKind, _: E) -> Self {
+//         Self::Nom(kind)
+//     }
+// }
